6b52e3cd89e49234df09602547ee57891536c953
mjrajput04 <manojvagadiya04@gmail.com>
2026-01-06 14:35:23 +0530

    fix

--- houseofsere-main/src/pages/admin/AdminUsers.tsx
+++ houseofsere-main/src/pages/admin/AdminUsers.tsx
@@ -167,10 +167,7 @@ const AdminUsers = () => {
           <h1 className="text-3xl font-bold text-gray-900">User Management</h1>
           <p className="text-gray-600 mt-1">Manage and monitor all system users</p>
         </div>
-        <Button className="bg-blue-600 hover:bg-blue-700 text-white">
-          <UserPlus className="h-4 w-4 mr-2" />
-          Add New User
-        </Button>
+        
       </div>
 
       {/* Stats Cards */}
